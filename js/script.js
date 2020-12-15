b9ea6edefacf221c16b47ad50abb40209b3c7feb
oat phattaraphon <phattaraphon.c@ku.th>
2020-12-16 02:43:26 +0700

    iniitial project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// let mainNav = document.getElementById('js-menu');
+// let navBarToggle = document.getElementById('menu');
+
+// navBarToggle.addEventListener('click', function() {
+//     mainNav.classList.toggle('active');
+// })
+
+// const backtotop = document.querySelector('.backtotop');
+
+// window.onscroll = function() {
+//     if (window.pageYOffset > 800) {
+//         backtotop.classList.add('show-backtotop');
+//     }
+//     else {
+//         backtotop.classList.remove('show-backtotop');
+//     }
+// }
