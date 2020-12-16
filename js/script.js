ac43264ff1fee018c4d10692cbdd2558ce613e0a
oat phattaraphon <phattaraphon.c@ku.th>
2020-12-16 19:53:49 +0700

    initial proj candidate-icweb

--- js/script.js
+++ js/script.js
@@ -1,17 +1,66 @@
-// let mainNav = document.getElementById('js-menu');
-// let navBarToggle = document.getElementById('menu');
+// Function for Overviews Section
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  const obj1 = document.getElementById('value1');
+  const obj2 = document.getElementById('value2');
+  const obj3 = document.getElementById('value3');
+  const obj4 = document.getElementById('value4');
+  animateValue(obj1, 0, 1054, 6000);
+  animateValue(obj2, 0, 154, 6000);
+  animateValue(obj3, 0, 59, 6000);
+  animateValue(obj4, 0, 34, 6000);
 
-// navBarToggle.addEventListener('click', function() {
-//     mainNav.classList.toggle('active');
-// })
 
-// const backtotop = document.querySelector('.backtotop');
+// Function for Great Work Section
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
 
-// window.onscroll = function() {
-//     if (window.pageYOffset > 800) {
-//         backtotop.classList.add('show-backtotop');
-//     }
-//     else {
-//         backtotop.classList.remove('show-backtotop');
-//     }
-// }
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
